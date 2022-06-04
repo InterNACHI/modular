eb364f0ae1d08291c2291f7485b086dd7eadb1c7
Chris Morrell <inxilpro@users.noreply.github.com>
2022-06-04 12:43:18 -0400

    More seeder adjustments

--- src/Console/Commands/Make/MakeSeeder.php
+++ src/Console/Commands/Make/MakeSeeder.php
@@ -3,16 +3,31 @@
 namespace InterNACHI\Modular\Console\Commands\Make;
 
 use Illuminate\Database\Console\Seeds\SeederMakeCommand;
+use Illuminate\Support\Str;
 
 class MakeSeeder extends SeederMakeCommand
 {
-	use Modularize;
+	use Modularize {
+		getPath as getModularPath;
+	}
+	
+	protected function getPath($name)
+	{
+		if ($module = $this->module()) {
+			$name = Str::replaceFirst($module->qualify('Database\\Seeders\\'), '', $name);
+			return $this->getModularPath($name);
+		}
+		
+		return parent::getPath($name);
+	}
 	
 	protected function replaceNamespace(&$stub, $name)
 	{
 		if ($module = $this->module()) {
-			$namespace = $module->qualify('Database\Seeders');
-			$stub = str_replace('namespace Database\Seeders;', "namespace {$namespace};", $stub);
+			if (version_compare($this->getLaravel()->version(), '9.6.0', '<')) {
+				$namespace = $module->qualify('Database\Seeders');
+				$stub = str_replace('namespace Database\Seeders;', "namespace {$namespace};", $stub);
+			}
 		}
 		
 		return parent::replaceNamespace($stub, $name);
