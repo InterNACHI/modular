883f0ad1addfcdf98f1863db243ac81a1714a3c7
Sophist <3001893+Sophist-UK@users.noreply.github.com>
2022-07-30 22:46:48 +0100

    Fixup tests

--- tests/Commands/Make/MakeCastTest.php
+++ tests/Commands/Make/MakeCastTest.php
@@ -11,29 +11,29 @@ class MakeCastTest extends TestCase
 {
 	use WritesToAppFilesystem;
 	use TestsMakeCommands;
-	
+
 	public function test_it_overrides_the_default_command(): void
 	{
 		$this->requiresLaravelVersion('9.2.0');
-		
+
 		$this->artisan('make:cast', ['--help' => true])
 			->expectsOutputToContain('--module')
 			->assertExitCode(0);
 	}
-	
+
 	public function test_it_scaffolds_a_cast_in_the_module_when_module_option_is_set(): void
 	{
 		$command = MakeCast::class;
 		$arguments = ['name' => 'JsonCast'];
-		$expected_path = 'src/Casts/JsonCast.php';
+		$expected_path = '/src/Casts/JsonCast.php';
 		$expected_substrings = [
 			'namespace Modules\TestModule\Casts',
 			'class JsonCast',
 		];
-		
+
 		$this->assertModuleCommandResults($command, $arguments, $expected_path, $expected_substrings);
 	}
-	
+
 	public function test_it_scaffolds_a_cast_in_the_app_when_module_option_is_missing(): void
 	{
 		$command = MakeCast::class;
@@ -43,7 +43,7 @@ class MakeCastTest extends TestCase
 			'namespace App\Casts',
 			'class JsonCast',
 		];
-		
+
 		$this->assertBaseCommandResults($command, $arguments, $expected_path, $expected_substrings);
 	}
 }
--- tests/Concerns/WritesToAppFilesystem.php
+++ tests/Concerns/WritesToAppFilesystem.php
@@ -7,71 +7,69 @@ use Illuminate\Filesystem\Filesystem;
 trait WritesToAppFilesystem
 {
 	protected $filesystem;
-	
+
 	protected $base_path;
-	
+
 	protected function filesystem(): Filesystem
 	{
 		if (null === $this->filesystem) {
 			$this->filesystem = new Filesystem();
 		}
-		
+
 		return $this->filesystem;
 	}
-	
+
 	protected function resolveApplication()
 	{
 		$this->base_path = null;
-		
+
 		return parent::resolveApplication();
 	}
-	
+
 	protected function copyStub(string $stub, string $destination): string
 	{
 		$destination = trim($destination, '/');
-		$destination = $this->getBasePath().$this->normalizeDirectorySeparators("/{$destination}");
-		
-		$stubs_directory = dirname(__FILE__, 2).'/stubs';
-		
+		$destination = $this->getBasePath().$this->normalizeDirectorySeparators("{$destination}");
+
+		$stubs_directory = str_replace('\\', '/', dirname(__DIR__, 1)).'/stubs';
+
 		$from = $this->normalizeDirectorySeparators("{$stubs_directory}/{$stub}");
 		$to = $this->normalizeDirectorySeparators("{$destination}/{$stub}");
-		
+
 		$this->filesystem()->ensureDirectoryExists($destination);
 		$this->filesystem()->copy($from, $to);
-		
+
 		return $to;
 	}
-	
+
 	protected function getBasePath()
 	{
 		if (null === $this->base_path) {
 			$this->filesystem()->copyDirectory(
 				parent::getBasePath(),
-				$this->base_path = sys_get_temp_dir().DIRECTORY_SEPARATOR.md5(microtime())
+				$this->base_path = str_replace('\\','/',sys_get_temp_dir()).'/'.md5(microtime())
 			);
 		}
-		
+
 		return $this->base_path;
 	}
-	
+
 	protected function getModulePath(string $module_name, string $path = '/', string $modules_root = 'app-modules'): string
 	{
 		return $this->getBasePath()
-			.DIRECTORY_SEPARATOR
+			.'/'
 			.$modules_root
-			.DIRECTORY_SEPARATOR
+			.'/'
 			.$module_name
 			.$this->normalizeDirectorySeparators($path);
 	}
-	
+
 	protected function normalizeDirectorySeparators(string $path): string
 	{
-		$path = trim(str_replace('/', DIRECTORY_SEPARATOR, $path), DIRECTORY_SEPARATOR);
-		
-		if ('' !== $path) {
-			$path = DIRECTORY_SEPARATOR.$path;
+		if (($path = trim($path, '/')) && (substr($path, 1, 1) !== ':')) {
+			$path = '/'.$path;
 		}
-		
+
 		return $path;
 	}
 }
