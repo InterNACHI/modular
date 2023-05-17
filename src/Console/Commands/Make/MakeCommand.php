bdcac801a02ec4140dab4ffe36412fba0d78cac7
Chris Morrell <inxilpro@users.noreply.github.com>
2023-05-17 12:08:27 -0400

    make:command BC

--- src/Console/Commands/Make/MakeCommand.php
+++ src/Console/Commands/Make/MakeCommand.php
@@ -14,7 +14,7 @@ class MakeCommand extends ConsoleMakeCommand
 		$stub = parent::replaceClass($stub, $name);
 		$module = $this->module();
 		
-		if ($module && ! $this->option('command')) {
+		if ($module && (! $this->option('command') || 'command:name' === $this->option('command'))) {
 			$cli_name = Str::of($name)->classBasename()->kebab();
 			
 			$find = [
