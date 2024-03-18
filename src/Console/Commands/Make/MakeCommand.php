f845ec5d567e112ae4a09ff52e2ae7d30acb9d21
Chris Morrell <inxilpro@users.noreply.github.com>
2024-03-18 14:29:56 -0400

    Replace command name before parent

--- src/Console/Commands/Make/MakeCommand.php
+++ src/Console/Commands/Make/MakeCommand.php
@@ -11,20 +11,19 @@ class MakeCommand extends ConsoleMakeCommand
 	
 	protected function replaceClass($stub, $name)
 	{
-		$stub = parent::replaceClass($stub, $name);
 		$module = $this->module();
 		
 		if ($module && (! $this->option('command') || 'command:name' === $this->option('command'))) {
 			$cli_name = Str::of($name)->classBasename()->kebab();
 			
 			$find = [
-				"'command:name'",
-				"'app:{$cli_name}'",
+				"command:name",
+				"app:{$cli_name}",
 			];
 			
-			$stub = str_replace($find, "'{$module->name}:{$cli_name}'", $stub);
+			$stub = str_replace($find, "{$module->name}:{$cli_name}", $stub);
 		}
 		
-		return $stub;
+		return parent::replaceClass($stub, $name);
 	}
 }
