c4169a7806887654904c03e5e11d1f437342e067
Eric Junker <eric.junker@dice.com>
2024-12-12 15:48:33 -0600

    Fix PHP 8.4 deprecations and add nullable type declaration for default null value

--- src/Exceptions/CannotFindModuleForPathException.php
+++ src/Exceptions/CannotFindModuleForPathException.php
@@ -6,7 +6,7 @@ use Throwable;
 
 class CannotFindModuleForPathException extends Exception
 {
-	public function __construct(string $path, Throwable $previous = null)
+	public function __construct(string $path, ?Throwable $previous = null)
 	{
 		parent::__construct("Unable to determine module for '{$path}'", 0, $previous);
 	}
