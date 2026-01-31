5ca44d7c6910cb89283ee7b0c39fab493b793b4c
Chris Morrell <inxilpro@users.noreply.github.com>
2026-01-30 23:19:09 -0500

    Simplify event listener test to verify actual event dispatching and message handling

--- tests/Plugins/EventsPluginTest.php
+++ tests/Plugins/EventsPluginTest.php
@@ -3,49 +3,27 @@
 namespace InterNACHI\Modular\Tests\Plugins;
 
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Str;
 use InterNACHI\Modular\Tests\Concerns\PreloadsAppModules;
 use InterNACHI\Modular\Tests\TestCase;
 use Modules\TestModule\Events\TestEvent;
+use Modules\TestModule\Listeners\TestEventListener;
 
 class EventsPluginTest extends TestCase
 {
 	use PreloadsAppModules;
-
+	
 	protected function defineEnvironment($app): void
 	{
-		$app['config']->set('app-modules.should_discover_events', true);
+		config()->set('app-modules.should_discover_events', true);
 	}
-
-	public function test_event_listeners_are_discovered(): void
-	{
-		$dispatcher = Event::getFacadeRoot();
-		$listeners = $dispatcher->getListeners(TestEvent::class);
-
-		$hasListener = false;
-		foreach ($listeners as $listener) {
-			if (is_string($listener) && str_contains($listener, 'TestEventListener')) {
-				$hasListener = true;
-				break;
-			}
-			if ($listener instanceof \Closure) {
-				$reflection = new \ReflectionFunction($listener);
-				$vars = $reflection->getStaticVariables();
-				if (isset($vars['listener']) && str_contains($vars['listener'], 'TestEventListener')) {
-					$hasListener = true;
-					break;
-				}
-			}
-		}
-
-		$this->assertTrue($hasListener, 'TestEventListener should be discovered for TestEvent');
-	}
-
+	
 	public function test_event_can_be_dispatched_and_listener_is_called(): void
 	{
-		Event::fake([TestEvent::class]);
-
-		TestEvent::dispatch();
-
-		Event::assertDispatched(TestEvent::class);
+		$message = Str::random();
+		
+		Event::dispatch(new TestEvent($message));
+		
+		$this->assertEquals($message, TestEventListener::$last_message);
 	}
 }
