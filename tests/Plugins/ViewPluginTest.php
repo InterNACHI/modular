51845bfbfe0a98a3e22ff8c414cb5770ea5570ce
Chris Morrell <inxilpro@users.noreply.github.com>
2026-01-30 23:35:05 -0500

    Normalize whitespace in ViewPluginTest

--- tests/Plugins/ViewPluginTest.php
+++ tests/Plugins/ViewPluginTest.php
@@ -9,24 +9,24 @@ use InterNACHI\Modular\Tests\TestCase;
 class ViewPluginTest extends TestCase
 {
 	use PreloadsAppModules;
-
+	
 	public function test_view_namespace_is_registered(): void
 	{
 		$this->assertTrue(View::exists('test-module::index'));
 	}
-
+	
 	public function test_view_can_be_rendered(): void
 	{
 		$content = View::make('test-module::index')->render();
-
+		
 		$this->assertStringContainsString('hello world', $content);
 	}
-
+	
 	public function test_view_finder_has_module_namespace(): void
 	{
 		$finder = $this->app['view']->getFinder();
 		$hints = $finder->getHints();
-
+		
 		$this->assertArrayHasKey('test-module', $hints);
 	}
 }
