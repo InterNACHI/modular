d0c958b6bd3b07aa881eff38955aab16f5dcbcf0
Chris Morrell <inxilpro@users.noreply.github.com>
2026-01-31 00:20:31 -0500

    Add boost support

---
+++ resources/boost/guidelines/core.blade.php
@@ -0,0 +1,4 @@
+## Modular
+
+- This a modular application. Each module is located in its own directory inside of `app-modules`.
+- IMPORTANT: Activate `modular` every time you're working with or creating a new module.
