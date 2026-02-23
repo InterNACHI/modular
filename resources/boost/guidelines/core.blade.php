5c7913331a3e002528be41920dbedad7ea763e83
Caleb White <cdwhite3@pm.me>
2026-02-23 08:59:03 -0600

    fix: boost skill does not use correct module directory (#133)
    * fix: boost skill does not use correct module directory
    
    The directory can be customized via a config setting, therefore it
    should not be hardcoded.
    
    * Code style
    
    ---------
    
    Co-authored-by: Chris Morrell <inxilpro@users.noreply.github.com>

--- resources/boost/guidelines/core.blade.php
+++ resources/boost/guidelines/core.blade.php
@@ -1,4 +1,4 @@
 ## Modular
 
-- This a modular application. Each module is located in its own directory inside of `app-modules`.
+- This a modular application. Each module is located in its own directory inside of `{{ config('app-modules.modules_directory') }}`.
 - IMPORTANT: Activate `modular` every time you're working with or creating a new module.
